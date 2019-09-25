d6beac6d1c2f56f161add8927aa08320c500f4d7
Glen Pegado <glenpegado@gmail.com>
2019-09-25 15:09:23 -0500

    Installed graphql and added first query

--- src/index.js
+++ src/index.js
@@ -4,18 +4,56 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+//Apollo
+import {ApolloProvider} from 'react-apollo';
+import {createHttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloClient, gql} from 'apollo-boost'; //smaller libraries with alot of functionalities
+
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+})
+
+//Local storage
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+client.query({
+  query: gql`
+  {
+    getCollectionsByTitle(title: "hats"){
+      id
+      title
+      items{
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+  `
+})
+.then(res => console.log(res))
+
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <Provider store={store}>
     <BrowserRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </BrowserRouter>
-  </Provider>,
+  </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
